a5438a062eb8fe7dbbdfaa7d4f2547e787931960
wjddns7828 <wjddns7828980902@gmail.com>
2023-05-23 14:29:39 +0900

    2023.05.23 임정운 첫파일

---
+++ src/main/java/com/jeong/edu/App.java
@@ -0,0 +1,13 @@
+package com.jeong.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
